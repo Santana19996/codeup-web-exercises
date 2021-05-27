7c8d038a45c593946959935f18012dbe4edc3ca4
Oscar Santana <oscarsantana2014@gmail.com>
2021-05-27 11:50:30 -0500

    added works

---
+++ js/external.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//... rest of javascript code
+
+console.log('Hello from JavaScript!');
+
+alert("Get out of my website");
+
+// the following line will show the OK/CANCEL confirm dialog
+var confirmed = confirm('Are you sure you want to disobey?');
+console.log(confirmed); // will be either true or false
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
