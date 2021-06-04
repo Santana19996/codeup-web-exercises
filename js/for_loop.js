42240855b0fd6c7371d193e165360d7d05553ea2
Oscar Santana <oscarsantana2014@gmail.com>
2021-06-04 11:10:57 -0500

    loop excercises

---
+++ js/for_loop.js
@@ -0,0 +1,30 @@
+//let Hiro = 7;
+//for (let ZeroTwo = 1; ZeroTwo <= 10; ++ZeroTwo)
+ //   console.log( Hiro + " * " +ZeroTwo +
+//        " = " + Hiro *
+    //    ZeroTwo +"");
+
+//Lets practice and make my own
+//let yagami = 5;
+//for (deathnote = 5; deathnote <= 20; ++deathnote)
+ //   console.log(yagami + "*" +deathnote +
+  //  "=" + yagami *
+   // deathnote +"");
+
+//lets practice one more so I can get the hang of it
+//I want to add a Letter and  number and number and have the number increase in increments.
+//let oscar = "a";
+//for (seatnumber = 1; seatnumber < 50; ++seatnumber )
+  //  console.log(oscar + "+" +seatnumber +
+  //  "=" + "oscar" *
+  //  "seatnumber" + "")
+
+for (let i = 0; i < 10; i++) {
+    const rnd = Math.floor(Math.random() * 180+20);
+    if (rnd % 2 === 0) {
+        console.log(rnd +  " is even");
+    }
+    else {
+        console.log(rnd + " is odd");
+    }
+}
---
+++ js/loop_classwork.js
@@ -0,0 +1,11 @@
+var message = "" ;
+    var howmany = prompt("How many?")
+
+    for (let index = 0; index <10; index++) {
+
+        message = message + index + "\n";
+
+    }
+    alert(message);
+
+    alert(index);
