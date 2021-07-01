06762dafe611befde03ffd5f6d997c523f64f1b2
Oscar Santana <oscarsantana2014@gmail.com>
2021-07-01 10:11:25 -0500

    first commit

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -1,9 +1,16 @@
 // TODO: Create a function named countSpaces that accepts a
 //  string and returns the total number of spaces in the string.
 
+// let quoteSample = "The quick brown fox jumps over the lazy dog.";
+// let alphabetRegex= /[a-z]/ig
+// let result = quoteSample.match(alphabetRegex)
+// i igngores, the g finds every chracter of it
+
 function countSpaces(x){
-   return x.match(/([\s]+)/g).length
-//g will find every occurence
+   return x.match(/([\s]+)/g).length // this was on stack overflow
+                                       //This actually makes more sense to me
+//g will find every occurence          //compared to a for loop solution
+
    //match returns the mathces obviously
    //s matches a single white space character
        //we know what .length does
@@ -11,7 +18,9 @@ function countSpaces(x){
      //  /([\s]+)/
    //+ is like a & similar to what i did in my practice
    // it matches character before + one or more times
+
 }
+
 
 
 // TODO: Add validation to function above,
