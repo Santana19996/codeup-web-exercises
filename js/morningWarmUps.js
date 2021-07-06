4d3b1896d9bf3350276b4ba5ca44ae222060c1a0
Oscar Santana <oscarsantana2014@gmail.com>
2021-07-06 09:31:23 -0500

    first commit

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -26,3 +26,34 @@ function countSpaces(x){
 // TODO: Add validation to function above,
 //  if the argument pass is not a string it should return false.
 console.log(countSpaces("4 4 4 4 4 4"))
+
+
+
+
+
+
+// July 6th 2021
+// TODO: Create a function named capitalizeAllNames
+//  that accepts an array of strings and
+//  returns the array with all of its elements capitalized.
+// Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"])
+// ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
+let list = ["bob", "Seth", "TOFU", "Toyota"];
+list = list.map(function(x){
+   return x.toUpperCase()
+})
+
+
+
+
+// TODO: Create a function named capitalizeFirstLetter
+//  that accepts an array of strings and
+//  returns the array with all of the first letters in
+//  each string being capitalized
+//  while having the rest of the string lowercase look at the example below for reference.
+// Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"])
+// ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
+
+let list2 = ["bob","Seth","TOFU","Toyota"]
+let captilizeFirstLetter =/[]/
+    let resultofregex =
