44944e9e0b86b713ca24a27bd7376c2abc60ef84
Oscar Santana <oscarsantana2014@gmail.com>
2021-07-07 09:24:20 -0500

    first commit

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -39,13 +39,17 @@ console.log(countSpaces("4 4 4 4 4 4"))
 // Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"])
 // ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
 let list = ["bob", "Seth", "TOFU", "Toyota"];
-list = list.map(function(x){
-   return x.toUpperCase()
-})
 
-
-
-
+function capitalizeAllNames (x){
+   return (list.map(a => a.toUpperCase()))
+}
+console.log(capitalizeAllNames())
+//This morning I had something completely different
+//take time and relax and it makes more sense
+//
+// function lower (x){
+//    return (list.map(a => a.toLowerCase()))
+//}
 // TODO: Create a function named capitalizeFirstLetter
 //  that accepts an array of strings and
 //  returns the array with all of the first letters in
@@ -53,7 +57,53 @@ list = list.map(function(x){
 //  while having the rest of the string lowercase look at the example below for reference.
 // Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"])
 // ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
+// let list2 = ["bob","Seth","TOFU","Toyota"]
+// function capitalizeFirstLetter (i){
+//    return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+//    }
+// console.log(capitalizeFirstLetter)
+//Cant get this one right...Figure it out on the weekend..
 
-let list2 = ["bob","Seth","TOFU","Toyota"]
-let captilizeFirstLetter =/[]/
-    let resultofregex =
+//Wednesday, July 7, 2021 at 9:03:46 AM
+// July 7th 2021
+//cre
+// TODO: Create a method on the object down below named getFullName
+//  that when called returns the users full name using the "this" key term.
+
+// Example: personOne.getFullName() ---- returns ----> "silvia floopertan"
+var personOne = {
+   firstName: "silvia",
+   lastName: "floopertan",
+   ageInYears: 34,
+   heightInInches: 65,
+   getFullName: function() {
+      return this.firstName + "" + this.lastName
+   }
+}
+console.log(personOne)
+
+//didnt even get passed this one
+//
+
+// TODO: Create a method on the object down below named getNicelyFormattedFullName
+//  that when called returns the
+//  users full name with both first letters in the names being capitalized
+//  look at example down below for more reference.
+// Example: personOne.getNicelyFormattedFullName() ---- returns ----> "Dan Valdarez"
+var personTwo = {
+   firstName: "dan",
+   lastName: "valdarez",
+   ageInYears: 61,
+   heightInInches: 73
+}
+// TODO: Create a method on the object below that gives us the behavior that the example implies.
+// Example: personThree.intro() ---- returns ---->
+// "Hello, My name is Arlene Martinez and I am 25 years old"
+var personThree = {
+   firstName: "arlene",
+   lastName: "martinez",
+   ageInYears: 25,
+   heightInInches: 62
+}
+
+
--- js/practice4me2.js
+++ js/practice4me2.js
@@ -3,16 +3,24 @@
 // this code will produce a console log every second
 // when count >= max, the interval is cancelled, and the logging will stop
 
-var count = 0;
-var max = 10;
-var interval = 300; // interval time in milliseconds
+// var count = 0;
+// var max = 10;
+// var interval = 300; // interval time in milliseconds
+//
+// var intervalId = setInterval(function () {
+//     if (count >= max) {
+//         clearInterval(intervalId);
+//         console.log('All done');
+//     } else {
+//         count++;
+//         console.log('Repeating this line ' + count);
+//     }
+// }, interval);
 
-var intervalId = setInterval(function () {
-    if (count >= max) {
-        clearInterval(intervalId);
-        console.log('All done');
-    } else {
-        count++;
-        console.log('Repeating this line ' + count);
-    }
-}, interval);
+
+
+
+
+function firstAndSecond(sequence){
+    return sequence[0] && sequence[1]
+}
