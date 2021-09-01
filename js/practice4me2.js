8431b1c8afe72004798c53265cc228a168a1a56a
Oscar Santana <oscarsantana2014@gmail.com>
2021-09-01 13:27:45 -0500

    Refactored WeatherMap with Loop

--- js/practice4me2.js
+++ js/practice4me2.js
@@ -19,12 +19,37 @@
 
 
 
+// function sayHello1(name) {
+//     return 'Hello, ' + name + '!';
+// }
+//
+//
+//
+// const sayHello = (name) => `Hello, ${name}!` //3 is good
+//
+// const sayHello2 = name => { 'Hello, ' + name + '!' } //not 2
+//
+// const sayHellofirst = (name) => { 'Hello, ' + name + '!' }
 
 
-function firstAndSecond(sequence){
-    return sequence[0] && sequence[1]
-}
+// const isDivisibleBy3 = n => n % 3 === 0;
+//
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+// console.log(numbers.filter(isDivisibleBy3));
+// console.log(numbers);
 
-
-
-
+// var originalString = "a"
+// var description = "b"
+//
+//
+// var myOutput = originalString + " is an example of " + description + " thinking.";
+//
+//
+// let myOutput2 = `${originalString} is an example of ${description} thinking.`;
+// const myOutput3 = `{originalString} is an example of {description} thinking.`;
+//
+//
+//
+// let myOutput4 = '${originalString} is an example of ${description} thinking.'
+//
