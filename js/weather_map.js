4dd4abb26285d8097d033fd92e017ad51cc53cd8
Oscar <oscarsantana2014@gmail.com>
2021-10-09 14:24:39 -0500

    test

---
+++ js/weather_map.js
@@ -0,0 +1,119 @@
+'use strict';
+
+$(document).ready(function () {
+    //global variables for long/lat
+    var mapLong = -98.4916;
+    var mapLat = 29.4252;
+
+    //establish new map
+    mapboxgl.accessToken = mapBoxAPIKey;
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [mapLong, mapLat],
+        zoom: 15
+    });
+//add marker and make it draggable
+    var marker = new mapboxgl.Marker({
+        draggable: true
+    })
+        .setLngLat([mapLong, mapLat])
+        .addTo(map);
+
+    function onDragEnd() { //move marker and collect new coordinates for where ever the marker drops
+        var lngLat = marker.getLngLat();
+
+
+        reverseGeocode(lngLat, mapBoxAPIKey).then(function (result) {
+            $('#currentCity').html('Current City: ' + result);
+            marker
+                .setLngLat([mapLong, mapLat])
+
+            map.flyTo({
+                center: [mapLong, mapLat],
+                essential: true
+            })
+
+        });
+        mapLong = lngLat.lng
+        mapLat = lngLat.lat
+        getWeather();
+    }
+
+
+
+
+
+    //grab weather using onecall
+    function getWeather() {
+        $.get(`https://api.openweathermap.org/data/2.5/onecall`, {
+            appid: OpenWeatherAPIKey,
+            lat: mapLat,
+            lon: mapLong,
+            units: "imperial"
+        }).done(function (data) { //after getting the weather, this is applying to the cards
+            $('#cardHolder').html(''); //need to create this first so when I can append later
+
+            data.daily.forEach(function (day, index) {
+                if (index < 5) {
+                    console.log(day);
+                    //cards with info. Used bootstrap card template
+                    var weatherCards = `<div class="card bg-light text-dark" id="cards" style="width: 18rem;">
+        <div class="card-header">
+        <!--figure out how to get just the day minus the GMT info-->      
+            ${new Date(day.dt * 1000).toDateString()}
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class=list-group-item id="temps">${day.temp.min.toFixed()}&#8457/${day.temp.max.toFixed()}&#8457</li>
+            <li class="list-group-item" id="icons"><img src="http://openweathermap.org/img/w/${day.weather[0].icon}.png"></li>
+            <li class="list-group-item">Description: <strong><em>${day.weather[0].description}</em></strong> <br>
+                Humidity: <strong>${day.humidity}&#37</strong>
+            </li>
+            <li class="list-group-item">Wind: <strong>${day.wind_speed.toFixed()}</strong></li>
+            <li class="list-group-item">Pressure: <strong>${day.pressure}</strong></li>
+        </ul>
+    </div>`
+                    $('#cardHolder').append(weatherCards); //add data onto cards, use template and fori loop to get the cards to populate
+
+                }
+            })
+        })
+    }
+
+
+
+//
+    marker.on('dragend', onDragEnd);
+    getWeather();
+
+
+//Find button goes to User's requested
+    $('#findButton').click(function (e) {
+        e.preventDefault();
+        var location = $('#search').val();
+        var lngLat = marker.getLngLat();
+        $('#currentCity').html('Current City: ' + location[0].toUpperCase() + location.substr(1)); //change Current City to reflect search
+
+        geocode(location, mapBoxAPIKey).then(function (result) {
+            console.log(result);
+            mapLong = result[0];
+            mapLat = result[1];
+
+            marker
+                .setLngLat([mapLong, mapLat])
+            map.flyTo({
+                speed: 0.5,
+                curve: 1,
+                center: [mapLong, mapLat],
+                essential: true
+            })
+
+        });
+        getWeather();
+    })
+    map.dragRotate.disable();
+
+// disable map rotation using touch rotation gesture
+    map.touchZoomRotate.disableRotation();
+})
+//end of document.ready
